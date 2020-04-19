edbb057a21c1bb371327c070965d3991e2e65ae0
shohincoding-com <salomov.shokhin@gmail.com>
2020-04-19 13:41:43 +0500

    creat new class converter and Method public converter. Added variable double

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HW_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Converter
+    {
+        public static double usd { get; set; }
+        public static double eur { get; set; }
+        public static double rub { get; set; }
+
+        public Converter(double C_us, double C_eu, double C_rb)
+        {
+            usd = C_us;
+            eur = C_eu;
+            rub = C_rb;
+        }
+    }
+}
